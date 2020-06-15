a567b0604c599518ff86472cd49f7ba3f7e4b17d
juanca limo <juancarlos.limo@hotmail.com>
2020-06-15 23:46:58 +0200

    create and new

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -7,4 +7,22 @@ class ArticlesController < ApplicationController
   def show
     @article = Article.find(params[:id])
   end
+
+  def new
+    @article = Article.new
+  end
+
+  def create
+    @article = Article.new(params.require(:article).
+      permit(:title, :description))
+    #render plain: @article.inspect
+    if @article.save
+      flash[:notice] = "Article was created successfully"
+      redirect_to article_path(@article)
+    # redirect_to @article --> tiene el mismo efecto
+    # que linea de arriba
+    else
+      render 'new'
+    end
+  end
 end
