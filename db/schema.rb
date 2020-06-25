854b978c2e8ee74aea6e7b0ef161d39de1f30375
juanca limo <juancarlos.limo@hotmail.com>
2020-06-25 22:51:35 +0200

    Add users table and user model

--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_06_15_101722) do
+ActiveRecord::Schema.define(version: 2020_06_25_204733) do
 
   create_table "articles", force: :cascade do |t|
     t.string "title"
     t.text "description"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "username"
+    t.integer "email"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
