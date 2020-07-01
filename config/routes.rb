abc36a6af5f72f11c413a304b4dd9c77391bdc71
juanca limo <juancarlos.limo@hotmail.com>
2020-07-01 18:36:11 +0200

    Add users create action

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,11 @@
 Rails.application.routes.draw do
   
+  get 'users/index'
   root to: 'pages#home'
   get 'about', to: 'pages#about'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :articles
+  get 'signup', to: 'users#new'
+  resources :users, except: [:new]
+  # post 'users', to: 'users#create'
 end
