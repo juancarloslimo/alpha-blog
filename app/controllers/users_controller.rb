a33a7d940e93960f754810a64acd38a5ca15fee4
juanca limo <juancarlos.limo@hotmail.com>
2020-07-01 19:44:55 +0200

    Adding edit template and update action

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,7 @@
 class UsersController < ApplicationController
 
+  before_action :set_article, only: [:edit, :update]
+
   def new
     @user = User.new
   end
@@ -14,9 +16,27 @@ class UsersController < ApplicationController
     end
   end
 
+  def edit
+    # When "Update account" button is submitted, it calls update methods sending just parameters and not @user, because of that in update method
+    # we have to find one more time which user will be updated. In other words, form sends data as params.
+  end
+
+  def update
+    if @user.update(user_params)
+      flash[:notice] = "Article was updated successfully"
+      redirect_to user_path(@user)
+    else
+      render 'edit'
+    end
+  end
+
   private 
   def user_params
     params.require(:user).permit(:username, :email, :password)
+  end
+
+  def set_article
+    @user = User.find(params[:id])
   end
 
 end
