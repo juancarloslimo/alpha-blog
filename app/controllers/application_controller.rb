71201851126871bc5a925b8d3e6a844ceb3d0bbd
juanca limo <juancarlos.limo@hotmail.com>
2020-07-02 21:31:31 +0200

    Fixing bug

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,5 @@
 class ApplicationController < ActionController::Base
-  helper_method :current_user, logged_in? # This makes our current_user and logged_in? methods visible and accesable for our controllers and all views. 
+  helper_method :current_user, :logged_in? # This makes our current_user and logged_in? methods visible and accesable for our controllers and all views. 
   def current_user
     @current_user ||= User.find(session[:user_id]) if session[:user_id]
   end
