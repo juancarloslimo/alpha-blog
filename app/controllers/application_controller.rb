b2f14e67ed4609d21c6b4e669e1e4e488ffaa4a9
juanca limo <juancarlos.limo@hotmail.com>
2020-07-02 19:05:39 +0200

    controller methods as helper methods

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,10 @@
 class ApplicationController < ActionController::Base
+  helper_method :current_user, logged_in? # This makes our current_user and logged_in? methods visible and accesable for our controllers and all views. 
+  def current_user
+    @current_user ||= User.find(session[:user_id]) if session[:user_id]
+  end
+
+  def logged_in?
+    !!current_user # !! <-- convert current_user in a boolean
+  end
 end
--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -17,7 +17,7 @@ class ArticlesController < ApplicationController
 
   def create
     @article = Article.new(article_params)
-    @article.user = User.first
+    @article.user = current_user
     #render plain: @article.inspect
     if @article.save
       flash[:notice] = "Article was created successfully"
