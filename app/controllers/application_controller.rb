087cbd52496c04b519763d3d4bdb38bc846e846b
juanca limo <juancarlos.limo@hotmail.com>
2020-07-02 23:36:20 +0200

    Restrict actions at controller level for Articles

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,4 +7,11 @@ class ApplicationController < ActionController::Base
   def logged_in?
     !!current_user # !! <-- convert current_user in a boolean
   end
+
+  def require_user
+    if !logged_in?
+      flash[:alert] = "You must be logged in to perfom that action"
+      redirect_to login_path
+    end
+  end
 end
