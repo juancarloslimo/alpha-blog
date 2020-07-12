f7c8f1046d9b4bb0c7d631887cf80db201d48faf
juanca limo <juancarlos.limo@hotmail.com>
2020-07-12 19:57:38 +0200

    add article_category table

--- app/models/category.rb
+++ app/models/category.rb
@@ -2,4 +2,6 @@ class Category < ApplicationRecord
   validates :name, presence: true,
     uniqueness: { case_sensitive: true },
     length: { minimum: 3, maximum: 10 }
+  has_many :article_categories
+  has_many :articles, through: :article_categories
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_07_07_120929) do
+ActiveRecord::Schema.define(version: 2020_07_12_171243) do
+
+  create_table "article_categories", force: :cascade do |t|
+    t.integer "article_id"
+    t.integer "category_id"
+  end
 
   create_table "articles", force: :cascade do |t|
     t.string "title"
