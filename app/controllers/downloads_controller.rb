582dcfdb27966a4aa3a3339724a6ae95234e10e5
juanca limo <juancarlos.limo@hotmail.com>
2020-07-14 23:24:45 +0200

    test streaming CSV file

---
+++ app/controllers/downloads_controller.rb
@@ -0,0 +1,41 @@
+require 'csv'
+class DownloadsController < ApplicationController
+
+
+  def download
+    # Tell Rack to stream the content
+    headers.delete("Content-Length")
+
+    # Don't cache anything from this generated endpoint
+    headers["Cache-Control"] ||= "no-cache"
+
+    # Tell the browser this is a CSV file
+    headers["Content-Type"] = "text/csv"
+
+    # Make the file download with a specific filename
+    headers["Content-Disposition"] = "attachment; filename=\"example.csv\""
+
+    # Don't buffer when going through proxy servers
+    headers["X-Accel-Buffering"] = "no"
+
+    # Set an Enumerator as the body
+    self.response_body = body
+
+    # Set the status to success
+    response.status = 200
+  end
+
+  
+  private
+
+  def body
+    Enumerator.new do |yielder|
+      20000.times do |num|
+        sleep 0.05
+        yielder << CSV.generate_line([num, "yay"])
+      end
+    end
+  end
+
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -11,6 +11,8 @@ Rails.application.routes.draw do
   get 'users/index'
   root to: 'pages#home'
   get 'about', to: 'pages#about'
+  get 'download', to: 'pages#download'
+  get 'downloads/download'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :articles
   get 'signup', to: 'users#new'
